57b9be7f2adcc80e090c690de4b0bf21e88e6443
Bart van Raaij <info@bartvanraaij.nl>
2024-12-04 09:30:11 +0100

    Day 04 scaffold

---
+++ src/04.rs
@@ -0,0 +1,55 @@
+use itertools::Itertools;
+use std::{env, fs};
+
+fn read_input_file(args: Vec<String>) -> String {
+    let default_input_filename = &String::from("input/04.txt");
+    let input_filepath: &str = args.get(1).unwrap_or(default_input_filename);
+    fs::read_to_string(input_filepath).expect("input file should be readable")
+}
+
+fn main() {
+    let input = read_input_file(env::args().collect());
+    let result_part_1 = part_1(&input);
+    println!("{:?}", result_part_1);
+
+    let result_part_2 = part_2(&input);
+    println!("{:?}", result_part_2);
+}
+
+fn part_1(input: &str) -> usize {
+    //input.split("\n").filter(|l| !l.is_empty()).map(|line| {});
+
+    0
+}
+
+fn part_2(input: &str) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const SAMPLE_DATA: &str = r#"
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+"#;
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(SAMPLE_DATA), 18);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2(SAMPLE_DATA), 0);
+    }
+}
